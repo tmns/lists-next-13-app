b50ec0ca5d431906b5f5566a495a750d96a135a9
tmns <tmn5@pm.me>
2023-08-07 19:31:52 +0800

    Use plain action for Note update

--- src/app/notes/_actions.ts
+++ src/app/notes/_actions.ts
@@ -1,39 +1,39 @@
 "use server";
 import type { Note } from "@prisma/client";
-import { action } from "lib/safe-action";
+import { getServerSession } from "next-auth";
 import { revalidatePath } from "next/cache";
+import { authOptions } from "pages/api/auth/[...nextauth]";
 import { prisma } from "server/db";
-import { z } from "zod";
 
-const updateNodeInput = z.object({
-  id: z.string(),
-  instanceId: z.string(),
-  content: z.string(),
-});
+type UpdatedNote = {
+  id: string;
+  instanceId: string;
+  content: string;
+};
 
-export const updateNote = action(
-  { input: updateNodeInput, withAuth: true },
-  async ({ id, instanceId, content }, session) => {
-    // Ensure that the requested note to edit belongs to the user making the request
-    const note = await prisma.note.findFirst({ where: { id } });
-
-    const belongsToUser = note?.userId === session.user.id;
-    if (!belongsToUser) {
-      throw new Error("You are not authorized to access that data.");
-    }
-
-    const updatedNote: Note = await prisma.note.update({
-      data: { content, instanceId, updatedAt: new Date().toISOString() },
-      where: { id },
-    });
-
-    console.log("broadcasting update for note", id, "from instance", instanceId);
-    const bc = new BroadcastChannel(`notes/${note.userId}`);
-    bc.postMessage({ note: updatedNote });
-    setTimeout(() => bc.close(), 5);
-
-    revalidatePath("/notes");
-
-    return updatedNote;
+export async function updateNote({ id, instanceId, content }: UpdatedNote) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return new Response("Not logged in", { status: 401 });
   }
-);
+
+  const note = await prisma.note.findFirst({ where: { id, userId: session.user.id } });
+
+  if (!note) {
+    throw new Error("You are not authorized to access that data.");
+  }
+
+  const updatedNote: Note = await prisma.note.update({
+    data: { content, instanceId, updatedAt: new Date().toISOString() },
+    where: { id },
+  });
+
+  console.log("broadcasting update for note", id, "from instance", instanceId);
+  const bc = new BroadcastChannel(`notes/${note.userId}`);
+  bc.postMessage({ note: updatedNote });
+  setTimeout(() => bc.close(), 5);
+
+  revalidatePath("/notes");
+
+  return updatedNote;
+}
